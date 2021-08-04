7616639c68039a5f6e5d7173e21992060db4f21e
Yueshia Vue <yueshiavue@Yueshias-MacBook-Pro.local>
2021-08-04 17:59:57 -0500

    Added server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static('/public'));
+
+app.use(require('./routes/api.js'));
+
+app.listen(PORT, () => {
+    console.log('Now listening');
+});
