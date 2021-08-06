9825cfd2be30109c8f90a5c52a92bd2115d1533c
Yueshia Vue <yueshiavue@Yueshias-MacBook-Pro.local>
2021-08-05 21:37:20 -0500

    Testing mongo

--- model/workouts.js
+++ model/workouts.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/workout', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
--- routes/api.js
+++ routes/api.js
@@ -19,32 +19,6 @@ router.put('/api/workouts/:id', ({ body, params }, res) => {
         res.json(err);
       });
   });
-  
-// router.put('/api/workouts/:id', async (req, res) => {
-//     // TODO: need to be fixed. Update is not updating correct data.
-//     let exercises = req.body;
-//     let id = req.params.id
-//     if(id !== 'undefined') {
-//         console.log('id ****',id);
-//         let recentWorkout = Workouts.findById(id);
-//         console.log('recentworkout',recentWorkout);
-//         // let updatedExercises = recentWorkout.exercises.map(data => {
-//         //     if (id === data.id) {
-//         //         return {...exercises}
-//         //     }
-//         //     return {data};
-//         // });
-
-//         // let updateData = {
-//         //     day: new Date().toString(),
-//         //     exercises: updatedExercises,
-//         //   }
-//         //   console.log('exerciseData',exercises);
-//         // let data = await Workouts.findByIdAndUpdate(id,updateData)
-//         // console.log('update data from mongo: ',data);
-//         // res.send(data);
-//     }
-// })
 
 router.get('/api/stats', async (req, res) => {
     let statsData = await Workouts.find({}).sort({_id: -1}).limit(1)
@@ -61,28 +35,6 @@ router.post('/api/workouts', ({body}, res) => {
         res.json(err);
       });
   });
-// router.post('/api/workouts', async (req, res) => {
-//     //add workout
-//     let data = req.body;
-//     let id = req.params.id;
-//     console.log('data add ***',data);
-//     console.log('id *****', id)
-//     if (id !== 'undefined') {
-//         let recentWorkout = Workouts.findById(id);
-//         recentWorkout.exercises = [...recentWorkout.exercises,data];
-//         if( Object.keys(data).length !== 0 && data.constructor === Object) {
-//             console.log(data);
-//             // let addData = await Workouts.insertMany([data]);
-//             let data = await Workouts.findByIdAndUpdate(id,updateData)
-//             console.log('addDATA ***',addData);
-//             // res.send(addData);
-//         }
-//     }
-// })
-
-// router.get('/api/workouts/range', async (req, res) => {
-//     //get workout
-// })
 
 router.get('/api/workouts/range', (req, res) => {
     Workouts.aggregate([
@@ -104,6 +56,5 @@ router.get('/api/workouts/range', (req, res) => {
         res.json(err);
       });
   });
-
 
 module.exports = router;
